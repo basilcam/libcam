72ce7ca5bb40ac364342476724bcdd2a275797b9
Cam <basilcam@posteo.de>
2022-01-18 10:13:35 -0600

    replace exit codes with macro

--- src/error.c
+++ src/error.c
@@ -1,17 +1,16 @@
 #include <cam/error.h>
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
 void cam_handle_error(char *message) {
     fprintf(stderr, "%s\n", message);
-    exit(0);
+    exit(EXIT_FAILURE);
 }
 
 void cam_handle_unix_error(char *message) {
     fprintf(stderr, "%s: %s\n", message, strerror(errno));
-    exit(0);
+    exit(EXIT_FAILURE);
 }
